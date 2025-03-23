ac11571e48c6181f328bae969b477bde43e6220e
arajas <arajas@taltech.ee>
2025-03-23 03:50:18 +0200

    DeliveryFeeRequest: added @AllArgsConstructor. DeliveryFeeResponse: added @Getter, @Setter.

--- src/main/java/com/example/fooddelivery/dto/DeliveryFeeResponse.java
+++ src/main/java/com/example/fooddelivery/dto/DeliveryFeeResponse.java
@@ -2,10 +2,13 @@ package com.example.fooddelivery.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter @Setter
 @AllArgsConstructor
 public class DeliveryFeeResponse {
-    private String message;
+    private String statusMessage;
     private Double totalFee;
 }
